e75a605ecaa8a5106b48f2d190b20448e57516c8
XTC <986524267@qq.com>
2020-09-13 14:00:20 +0800

    坐标test

--- SOURCE/test.c
+++ SOURCE/test.c
@@ -14,10 +14,21 @@ date:
 //使用时保证清除以下每一个注释，别忘了加进工程噢（我忘了*1）
 
 #include "common.h"
-
+/**************测试记得删除！！**********************/
+#define X0 516		//原点x坐标
+#define Y0 328.5	//原点y坐标
+#define SIZE 70		//六边形弦长，非边长！！！！
+/****************88***********************************/
 int test(void)
 {
-	
+	/*******/
+	int n1, n2;
+	int i = 0;
+	char str1[10];
+	char str2[10];
+	char str3[10];
+	char str4[10];
+	/*******/
 	clrmous(MouseX, MouseY);
 	test_draw();
 
@@ -29,6 +40,20 @@ int test(void)
 			return EXIT;
 		}
 		//以下是此页面核心函数
+		
+		if(mouse_press(0, 0, 1024, 768) == 1)
+		{
+			xy_tran((float)MouseX, (float)MouseY, &n1, &n2);
+			itoa(n1, str1, 10);
+			itoa(n2, str2, 10);
+			itoa(MouseX, str3, 10);
+			itoa(MouseY, str4, 10);
+			Outtext(10,0 , str1, 16, 30, 0);
+			Outtext(10,30, str2, 16, 30, 0);
+			Outtext(10,60 , str3, 16, 30, 0);
+			Outtext(10,90, str4, 16, 30, 0);
+			i += 30;
+		}
 		
 	}
 	
@@ -43,4 +68,25 @@ void test_draw()
 
 	//记得画右上角的叉叉
 	
+}
+
+/**********************测试坐标转换 记得删除*********************************/
+
+
+void xy_tran(int x, int y, int *n1, int *n2)
+{
+	float x2, y2;
+	(float)x;
+	(float)y;
+	x2 = (x - X0) + (y - Y0) * (18/35);
+	y2 = (y - Y0) * 39.357 / 35;
+	*n1 = (x2 - (y - Y0) *(18/35)) / SIZE + 1;
+	*n2 = (y2 ) / SIZE;
+	//search(x2, y2, n1, n2);
+}
+
+void search(float x2, float y2, int *n1, int *n2)
+{
+	*n1 = (x2 ) / SIZE+1;
+	*n2 = (y2 ) / SIZE;
 }
