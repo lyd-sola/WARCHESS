99dbabbceb55669619c72335b03d8cb050da3e9c
XTC <986524267@qq.com>
2020-09-16 02:29:04 +0800

    修改六边形坐标系的建立方式； 删除test页面的函数

--- SOURCE/test.c
+++ SOURCE/test.c
@@ -64,55 +64,6 @@ int test(void)
 		}
 		//以下是此页面核心函数
 		
-		if (mouse_press(0, 0, 20, 20) == 1)
-		{
-			for (i = 1; i <= 13;i++)
-			{
-				for (j = 1; j <= 13; j++)
-				{
-					xy(i, j, &x_tmp, &y_tmp);
-					rectangle64k(x_tmp - 10, y_tmp - 10, x_tmp + 10, y_tmp + 10, 0);
-					/*itoa(x_tmp, str1, 10);
-					itoa(y_tmp, str2, 10);
-					strcat(str5, str1);
-					strcat(str5, " , ");
-					strcat(str5, str2);
-					strcat(str5, ")");
-					Outtextxx(x_tmp - 30, y_tmp, x_tmp + 30, str5, 16, 0);*/
-				}
-			}
-			delay(1000);
-			test_draw();
-		}
-		
-		if (mouse_press(0, 748, 20, 768) == 1)
-		{
-			for (i = 0; i <= 25;i++)
-			{
-				for (j = 0; j <= 13; j++)
-				{
-					rectangle64k(X0, Y0, X0 + i * WIDTH, Y0 + j * LENGTH, 0);
-				}
-			}
-		}
-
-		if(mouse_press(20, 20, 1024, 768) == 1)
-		{
-			//xy_tran((float)MouseX, (float)MouseY, &n1, &n2);
-			//itoa(n1, str1, 10);
-			//itoa(n2, str2, 10);
-			//itoa(MouseX, str3, 10);
-			//itoa(MouseY, str4, 10);
-			///*Outtext(10,0 , str1, 16, 30, 0);
-			//Outtext(10,30, str2, 16, 30, 0);
-			//Outtext(10,60 , str3, 16, 30, 0);
-			//Outtext(10,90, str4, 16, 30, 0);*/
-			xy_tran((float)MouseX, (float)MouseY, &n1, &n2);
-			pos1 = center_xy(n1, n2);
-			Clrmous();
-			Icon_builder(pos1, 1);
-			i += 30;
-		}
 	}	
 }
 
