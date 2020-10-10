e3bebf4a2bc9a8cf4078fd974fb109d6fab5b0c9
lyd_sola <1359071901@qq.com>
2020-10-10 12:03:32 +0800

    将所有按钮函数移入buttons.c 注意：将一个按钮绘制函数和功能函数放一起，绘制函数函数名为xxx_btn()（可稍微发挥） 功能函数为xxx_btn_fun()（使用模板）

---
+++ HEADS/buttons.h
@@ -0,0 +1,37 @@
+/**********************************************************
+Copyright(c)  2020 刘云笛、陈旭桐 	WARCHESS战棋
+file_name:buttons.h
+Author:刘云笛、陈旭桐
+Version:
+Description:按钮函数
+				
+
+Function List：
+	
+**********************************************************/
+#ifndef	_BUTTONS_H_
+#define	_BUTTONS_H_
+
+#include "struct.h"
+
+void Button(int y1, char* s, int color, int color2);
+int Sharp_button(int y0, char* s, char* s_change, int color, int color2);
+
+void attack_button(char* s, int color);//攻击按钮
+void stay_button(char* s, int color);//驻扎按钮
+void move_button(int color);//移动按钮
+void del_button(int color);//删除按钮
+void nextr_button(int color);//下一回合按钮
+int atk_btn_fun(char* s, int color, int color_c);
+int stay_btn_fun(char* s, int color, int color_c);
+int nxt_btn_fun(int color, int color_c);
+
+void rect_button(int x1, int y1, int x2, int y2, char* s, int color); //方形按钮
+void rect_btn_frame(int x1, int y1, int x2, int y2, int color);
+int rec_btn_fun(int x1, int y1, int x2, int y2, int color);
+
+void battle_exit(int x, int y);
+
+
+
+#endif
